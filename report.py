d359e5ea204ddc4e8b009ee4f3b0b86fa6ac3d16
Marcos Pasqualino <marcos.pasqualino@gmail.com>
2012-09-04 23:26:18 -0300

    Generic find interdependencies analysis and find applications interdependencies

--- report.py
+++ report.py
@@ -13,11 +13,16 @@ application_re = re.compile('.*Main$')
 
 def analyze_graph(G):
     print 'Analyzing graph'
-    find_module_interdependendies(G)
+
+    print '\nModule interdependencies:'
+    find_interdependendies(G, is_module)
+
+    print '\nApplication interdependencies:'
+    find_interdependendies(G, is_application)
 
 
-def find_module_interdependendies(G):
-    modules = filter(is_module, G.nodes())
+def find_interdependendies(G, filter_function):
+    modules = filter(filter_function, G.nodes())
 
     for source, target in itertools.permutations(modules, 2):
         if nx.has_path(G, source, target):
@@ -27,6 +32,10 @@ def find_module_interdependendies(G):
 
 def is_module(node):
     return not module_re.match(node) is None
+
+
+def is_application(node):
+    return not application_re.match(node) is None
 
 
 def main():
